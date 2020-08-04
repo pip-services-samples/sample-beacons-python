4e56501e8022977639217df733e09123c1ab7c22
banalna <judas.priest999@gmail.com>
2020-08-04 03:26:04 +0300

    fix app.py

--- step8/process/app.py
+++ step8/process/app.py
@@ -8,9 +8,16 @@
     :copyright: Conceptual Vision Consulting LLC 2015-2016, see AUTHORS for more details.
     :license: MIT, see LICENSE for more details.
 """
+import sys
+import traceback
+import os
 
 from pip_services3_components.log import ConsoleLogger
-from ..container.BeaconsProcess import BeaconsProcess
+
+# add parent directory to path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+from src.container.BeaconsProcess import BeaconsProcess
 
 if __name__ == '__main__':
     runner = BeaconsProcess()
@@ -19,5 +26,5 @@ if __name__ == '__main__':
         runner.run()
     except Exception as ex:
         ConsoleLogger().fatal("Beacons", ex, "Error: ")
-        print(traceback.format_exc(ex))
-        sys.stderr.write(str(e) + '\n')
+        print(traceback.format_exc())
+        sys.stderr.write(str(ex) + '\n')
